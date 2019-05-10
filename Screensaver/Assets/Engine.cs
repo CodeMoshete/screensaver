0bd2ef773b613af6fbd677297894571fc9ceed18
Ben Mosher <codemoshete@gmail.com>
2019-05-10 16:40:19 -0700

    Add project base state

---
+++ Screensaver/Assets/Engine.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class Engine : MonoBehaviour
+{
+    public List<Camera> AvailableCameras;
+
+	void Start () 
+	{
+        int numDisplays = Mathf.Min(AvailableCameras.Count, Display.displays.Length);
+
+        for (int i = 0; i < numDisplays; ++i)
+        {
+            Display currentDisplay = Display.displays[i];
+            currentDisplay.Activate();
+            AssignRandomCamera(i);
+        }
+	}
+
+    private void AssignRandomCamera(int displayNum)
+    {
+        int cameraIndex = Random.Range(0, AvailableCameras.Count);
+        Camera cam = AvailableCameras[cameraIndex];
+        cam.gameObject.SetActive(true);
+        AvailableCameras.RemoveAt(cameraIndex);
+        cam.targetDisplay = displayNum;
+    }
+	
+	void Update () 
+	{
+		if (Input.anyKey || Input.GetAxis("Mouse X") > 0 || Input.GetAxis("Mouse Y") > 0)
+		{
+			Application.Quit ();
+		}
+	}
+}
---
+++ Screensaver/Assets/PlayDefaultAnimation.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public enum AvailableAnimations
+{
+    Enterprise,
+    Defiant,
+    Intrepid
+}
+
+public class PlayDefaultAnimation : MonoBehaviour
+{
+    public AvailableAnimations Animation;
+
+	void Start ()
+    {
+        Animator anim = GetComponent<Animator>();
+
+		switch(Animation)
+        {
+            case AvailableAnimations.Enterprise:
+                anim.SetTrigger("Enterprise");
+                break;
+            case AvailableAnimations.Defiant:
+                anim.SetTrigger("Defiant");
+                break;
+            case AvailableAnimations.Intrepid:
+                anim.SetTrigger("Intrepid");
+                break;
+        }
+	}
+}
